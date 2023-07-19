3c965546cc48c84d86b66cdbcab99bee15acd938
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-19 15:01:56 +0300

    ADD: ThemeProvider

---
+++ src/assets/styles/variables.js
@@ -0,0 +1,26 @@
+export const variables = Object.freeze({
+    background: {
+        color: '#FFFFFF',
+        lightBlue: '#CCE4FB',
+        accentBlue: '#CCE4FB',
+        yellow: '#FFC107'
+    },
+    color: {
+        main: '#111111',
+        yellow: '#FFC107',
+        blue: '#54ADFF',
+        wight: '#FEF9F9',
+        grey: '#888888',
+        error: '#F43F5E',
+        success: '#00C3AD'
+    },
+    border: {
+        blue: '2px solid #54ADFF',
+        yellow: '2px solid #FFC107',
+        error: '#F43F5E',
+        success: '#00C3AD',
+        radius: '40px',
+        shadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', 
+        
+    },
+})
