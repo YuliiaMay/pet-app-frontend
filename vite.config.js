835e2d9b8b0ffa4aeed4b81050b4792056f7de02
Artem <113770041+Artemdream@users.noreply.github.com>
2023-07-20 15:30:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pet-app-frontend/"
-})
+  base: "/pet-app-frontend",
+  server: {
+    open: "/pet-app-frontend",
+  },
+});
