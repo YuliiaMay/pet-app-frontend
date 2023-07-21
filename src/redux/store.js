ece6b1be10cf087f20763dadfe1214044e1fbfcc
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-21 22:59:05 +0300

    ADD: auth slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from "./authSlice/authSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    })
+];
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
+
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: persistReducer(authPersistConfig, authReducer),
+    },
+    middleware,
+    // devTools: process.env.NODE_ENV === 'development',
 });
 
+export const persistor = persistStore(store);
