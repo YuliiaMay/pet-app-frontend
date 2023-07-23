b65d45c06ebf6e6b91cea2b63ffa4ade8f641262
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-07-23 23:32:27 +0300

    www2

--- src/components/Notices/NoticesFilters/NoticesFilters.jsx
+++ src/components/Notices/NoticesFilters/NoticesFilters.jsx
@@ -7,30 +7,32 @@ import {
 } from "./NoticesFilters.style";
 import { FiPlus } from "react-icons/fi";
 import { useAuth } from "../../../hooks/useAuth";
-
+import { ResponsiveContainer } from "../../../assets/styles/ResponsiveContainer";
 export const NoticesFilters = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <Section>
-      <Container>
-        <ButtonFilters to="/notices/sell">sell</ButtonFilters>
-        <ButtonFilters to="/notices/lost-found">lost/found</ButtonFilters>
-        <ButtonFilters to="/notices/for-free">in good hands</ButtonFilters>
+    <ResponsiveContainer>
+      <Section>
+        <Container>
+          <ButtonFilters to="/notices/sell">sell</ButtonFilters>
+          <ButtonFilters to="/notices/lost-found">lost/found</ButtonFilters>
+          <ButtonFilters to="/notices/for-free">in good hands</ButtonFilters>
 
-        {isLoggedIn && (
-          <>
-            <ButtonFilters to="/notices/favorite">favorite ads</ButtonFilters>
-            <ButtonFilters to="/notices/own">my ads</ButtonFilters>
-          </>
-        )}
-      </Container>
-      <ButtonAdd to="/add-pet">
-        Add Pet
-        <BtnIcon>
-          <FiPlus />
-        </BtnIcon>
-      </ButtonAdd>
-    </Section>
+          {isLoggedIn && (
+            <>
+              <ButtonFilters to="/notices/favorite">favorite ads</ButtonFilters>
+              <ButtonFilters to="/notices/own">my ads</ButtonFilters>
+            </>
+          )}
+        </Container>
+        <ButtonAdd to="/add-pet">
+          Add Pet
+          <BtnIcon>
+            <FiPlus />
+          </BtnIcon>
+        </ButtonAdd>
+      </Section>
+    </ResponsiveContainer>
   );
 };
