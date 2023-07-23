dfbb2e5431d9dc04eac75b4d6bfbb7a9eb58d8ab
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-07-23 22:10:36 +0300

    fix2

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,13 +1,11 @@
 import { NoticesSearch } from "../../components/Notices/NoticesSearch/NoticesSearch";
 import { NoticesFilters } from "../../components/Notices/NoticesFilters/NoticesFilters";
-import { NoticesPetCardList } from "../../components/Notices/NoticesPetsCardList/NoticesPetCard";
 
 const NoticesPage = () => {
   return (
     <>
       <NoticesSearch />
       <NoticesFilters />
-      <NoticesPetCardList />
     </>
   );
 };
