62c63de0db9a2bf2e115a01d09a724a4a016ec9e
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-07-23 23:02:50 +0300

    qqqq

---
+++ src/components/Notices/NoticesPetsDetail/NoticesPetDetail.jsx
@@ -0,0 +1,116 @@
+import { Icon } from "../../Icon/Icon";
+import {
+  Div,
+  ModalLayout,
+  ModalCardWrapper,
+  ButtonClose,
+  ModalCard,
+  ModalCardInfo,
+  ModalCardImage,
+  Image,
+  SpanStatus,
+  ModalCardText,
+  Title,
+  Table,
+  Tbody,
+  Tr,
+  TdName,
+  TdValue,
+  Link,
+  Text,
+  Comments,
+  ButtonsWrapper,
+  Button,
+  ButtonTextAdd,
+  ButtonLinkContact,
+  ButtonTextContact,
+} from "./NoticesPetDetail.styled";
+
+export const NoticeCardDetail = () => {
+  const item = {};
+  return (
+    <>
+      <Div>
+        <ModalLayout>
+          <ModalCardWrapper>
+            <ButtonClose>
+              <Icon
+                iconName={"icon-cross"}
+                width={"24px"}
+                height={"24px"}
+                stroke={"#54ADFF"}
+                fill={"#54ADFF"}
+              />
+            </ButtonClose>
+            <ModalCard>
+              <ModalCardInfo>
+                <ModalCardImage>
+                  <Image src={item.url} alt="pet" loading="lazy"></Image>
+                  <SpanStatus>{item.status}</SpanStatus>
+                </ModalCardImage>
+                <ModalCardText>
+                  <Title>AAAAAAAAAAAAAAAA</Title>
+                  <Table>
+                    <Tbody>
+                      <Tr>
+                        <TdName>Name: </TdName>
+                        <TdValue>{item.name}</TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>Birthday: </TdName>
+                        <TdValue>{item.birthday}</TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>Type: </TdName>
+                        <TdValue>{item.breed}</TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>Place: </TdName>
+                        <TdValue>{item.place}</TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>The sex: </TdName>
+                        <TdValue>{item.sex}</TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>Email: </TdName>
+                        <TdValue>
+                          <Link href={"mailto:" + item.email}>
+                            {item.email}
+                          </Link>
+                        </TdValue>
+                      </Tr>
+                      <Tr>
+                        <TdName>Phone: </TdName>
+                        <TdValue>
+                          <Link href={"tel:" + item.phone}>{item.phone}</Link>
+                        </TdValue>
+                      </Tr>
+                    </Tbody>
+                  </Table>
+                </ModalCardText>
+              </ModalCardInfo>
+              <Text>
+                <Comments>Comments:</Comments> {item.comments}
+              </Text>
+              <ButtonsWrapper>
+                <Button>
+                  <ButtonTextAdd>Add to</ButtonTextAdd>
+                  <Icon
+                    iconName={"icon-heart"}
+                    width={"24px"}
+                    height={"24px"}
+                    stroke={"#fff"}
+                  />
+                </Button>
+                <ButtonLinkContact>
+                  <ButtonTextContact>Contact</ButtonTextContact>
+                </ButtonLinkContact>
+              </ButtonsWrapper>
+            </ModalCard>
+          </ModalCardWrapper>
+        </ModalLayout>
+      </Modal>
+    </>
+  );
+};
