df3751691eb4370e872a66064c3ea7b897cc3cab
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-24 20:42:10 +0300

    fix

--- src/components/Forms/AddPetForm/AddPetMultiStepForm.jsx
+++ src/components/Forms/AddPetForm/AddPetMultiStepForm.jsx
@@ -1,6 +1,6 @@
 const AddPetMultiStepForm = () => {
     return (
-
+        
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { store } from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/pet-app-frontend">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
