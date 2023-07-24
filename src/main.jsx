b6e9797d6ceaf3852ee86ff71f7df416d1c8ce6c
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-24 21:12:39 +0300

    ADD: BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle, FontStyles } from "./assets/styles";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
@@ -9,9 +9,9 @@ import { store } from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename="/pet-app-frontend">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
     <GlobalStyle />
     <FontStyles />
