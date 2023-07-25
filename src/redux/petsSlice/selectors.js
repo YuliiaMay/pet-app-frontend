e50b5e2b668469fa86cc37629141b300d1a43239
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-25 19:13:30 +0300

    ADD: stepper

--- src/redux/petsSlice/selectors.js
+++ src/redux/petsSlice/selectors.js
@@ -1,15 +1,18 @@
 // FormStage
 // for previous button
-export const selectCurrentStage = state => state.FormStage; 
+export const selectCurrentStage = state => state.pet.FormStage; 
 
 // OptionForm
-export const selectCategory = state => state.OptionForm.category;
+export const selectCategory = state => state.pet.OptionForm.category;
 
 // DataForm 
-export const selectName = state => state.DataForm.name;
-export const selectBirthday = state => state.DataForm.birthday;
-export const selectbreed = state => state.DataForm.breed;
+export const selectName = state => state.pet.DataForm.name;
+export const selectBirthday = state => state.pet.DataForm.birthday;
+export const selectbreed = state => state.pet.DataForm.breed;
 
 // MoreInfoForm 
-export const selectAvatarURL = state => state.MoreInfoForm.imgUrl;
-export const selectComment = state => state.MoreInfoForm.text;
+export const selectAvatarURL = state => state.pet.MoreInfoForm.imgUrl;
+export const selectComment = state => state.pet.MoreInfoForm.text;
+
+// PetData
+export const selectAllPetData = state => state.pet;
