07581edb86193c2ae49c5adf4fcc794d40507a20
AlexRose2603 <alexandra26korchak@gmail.com>
2023-07-25 13:32:31 +0300

    burger

--- src/components/SharedLayout/Burger/Burger.jsx
+++ src/components/SharedLayout/Burger/Burger.jsx
@@ -8,53 +8,94 @@ import {
   BurgerLogInBtn,
   BurgerBtnWrapper,
   BurgerRegisterBtn,
-  CloseBtn,
+  LogoutMobBtn,
+  LogoutTabBtn,
+  BurgerBtnTabWrapper,
+  LoginTabBox,
 } from "./Burger.styled";
 import BurgerNavigationItem from "./BurgerNavigationItem";
 import bglogo from "../Logo/LogoPet.jpg";
 import { NavLink } from "react-router-dom";
 import { ResponsiveContainer } from "../../../assets/styles/ResponsiveContainer";
 import { Icon } from "../../Icon/Icon";
+import { useAuth } from "../../../hooks/useAuth";
+import UserNav from "../UserNav/UserNav";
 
 export const Burger = ({ isOpen, toggleMenu }) => {
+  const { isLoggedIn } = useAuth();
   return (
     <>
       {isOpen ? (
         <ResponsiveContainer>
           <BurgerContainer>
             <BurgerBar>
-              <NavLink to="/main">
+              <NavLink to="/main" onClick={toggleMenu}>
                 <img src={bglogo} alt="logotype" />
               </NavLink>
-              <div>
-                <CloseBtn onClick={toggleMenu} />
-              </div>
-
-              {/* <Icon
-                onClick={toggleMenu}
-                iconName={"icon-cross"}
-                width={"24px"}
-                height={"24px"}
-                stroke={"#ffc107"}
-                fill={"#ffc107"}
-              /> */}
-            </BurgerBar>
-            <BurgerMenu>
-              <BurgerBtnWrapper>
-                <BurgerLogInBtn to="/login">
-                  Log IN
+              <LoginTabBox>
+                {isLoggedIn ? (
+                  <>
+                    <LogoutTabBtn to="/logout" onClick={toggleMenu}>
+                      Log out
+                      <Icon
+                        iconName={"icon-logout"}
+                        width={"24px"}
+                        height={"24px"}
+                        stroke={"#ffffff"}
+                        fill={"#ffffff"}
+                      />
+                    </LogoutTabBtn>
+                    <div onClick={toggleMenu}>
+                      <UserNav />
+                    </div>
+                  </>
+                ) : (
+                  <BurgerBtnTabWrapper>
+                    <BurgerLogInBtn to="/login" onClick={toggleMenu}>
+                      Log IN
+                      <Icon
+                        iconName={"icon-pawprint"}
+                        width={"24px"}
+                        height={"24px"}
+                        stroke={"#ffffff"}
+                        fill={"#ffffff"}
+                      />
+                    </BurgerLogInBtn>
+                    <BurgerRegisterBtn to="/register" onClick={toggleMenu}>
+                      Registration
+                    </BurgerRegisterBtn>
+                  </BurgerBtnTabWrapper>
+                )}
+                <div onClick={toggleMenu}>
                   <Icon
-                    iconName={"icon-pawprint"}
+                    iconName={"icon-cross"}
                     width={"24px"}
                     height={"24px"}
-                    stroke={"#ffffff"}
-                    fill={"#ffffff"}
+                    stroke={"#ffc107"}
+                    fill={"#ffc107"}
                   />
-                </BurgerLogInBtn>
-                <BurgerRegisterBtn to="/register">
-                  Registration
-                </BurgerRegisterBtn>
-              </BurgerBtnWrapper>
+                </div>
+              </LoginTabBox>
+            </BurgerBar>
+
+            <BurgerMenu>
+              {!isLoggedIn && (
+                <BurgerBtnWrapper>
+                  <BurgerLogInBtn to="/login" onClick={toggleMenu}>
+                    Log IN
+                    <Icon
+                      iconName={"icon-pawprint"}
+                      width={"24px"}
+                      height={"24px"}
+                      stroke={"#ffffff"}
+                      fill={"#ffffff"}
+                    />
+                  </BurgerLogInBtn>
+                  <BurgerRegisterBtn to="/register" onClick={toggleMenu}>
+                    Registration
+                  </BurgerRegisterBtn>
+                </BurgerBtnWrapper>
+              )}
               <BurgerNavList>
                 <BurgerNavigationItem to="/news" toggleMenu={toggleMenu}>
                   News
@@ -66,7 +107,31 @@ export const Burger = ({ isOpen, toggleMenu }) => {
                   Our friends
                 </BurgerNavigationItem>
               </BurgerNavList>
+              {isLoggedIn && (
+                <LogoutMobBtn to="/logout" onClick={toggleMenu}>
+                  Log out
+                  <Icon
+                    iconName={"icon-logout"}
+                    width={"24px"}
+                    height={"24px"}
+                    stroke={"#ffffff"}
+                    fill={"#ffffff"}
+                  />
+                </LogoutMobBtn>
+              )}
             </BurgerMenu>
+            {/* {isLoggedIn && (
+              <LogoutMobBtn to="/logout" onClick={toggleMenu}>
+                Log out
+                <Icon
+                  iconName={"icon-logout"}
+                  width={"24px"}
+                  height={"24px"}
+                  stroke={"#ffffff"}
+                  fill={"#ffffff"}
+                />
+              </LogoutMobBtn>
+            )} */}
           </BurgerContainer>
         </ResponsiveContainer>
       ) : (
