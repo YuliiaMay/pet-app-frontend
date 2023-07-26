b4aa2407aea6476d6d4944c741823d06e722828f
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-26 19:53:44 +0300

    ADD: personal details form

--- src/components/Forms/AddPetForm/PersonalDetailsForm/DetailsForm.styled.js
+++ src/components/Forms/AddPetForm/PersonalDetailsForm/DetailsForm.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const DetailsFormWrapper = styled.div`
+export const Form = styled.form`
     display: flex;
     flex-direction: column;
     margin-bottom: 40px;
