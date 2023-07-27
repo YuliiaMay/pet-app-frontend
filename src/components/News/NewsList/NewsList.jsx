356527ed6f274774c96b82f09ca0fa16aa22f081
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-07-27 09:52:50 +0300

    add loader and fix

--- src/components/News/NewsList/NewsList.jsx
+++ src/components/News/NewsList/NewsList.jsx
@@ -1,14 +1,16 @@
 import { ResponsiveContainer } from "../../../assets/styles/ResponsiveContainer";
 import { List } from "./NewsList.styled";
 import { fetchNews } from "../../../redux/newsSlice/operations";
+import { NewsItem } from "../NewsItem/NewsItems";
+import { Loader } from "../../Loader/Loader";
+
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-import { NewsItem } from "../NewsItem/NewsItems";
-
 export default function NewsList() {
   const [newsData, setNewsData] = useState([]);
-  const [fetching, setFetching] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,25 +21,17 @@ export default function NewsList() {
       } catch (error) {
         console.log(error.message);
       }
-      setFetching(false);
+      setIsLoading(false);
     };
 
-    if (fetching) {
-      fetchData();
-    }
-
-    return () => {
-      setFetching(false);
-    };
-  }, [dispatch, fetching]);
+    fetchData();
+  }, [dispatch]);
 
   const dataNews = newsData.slice(0, 20);
 
   return (
     <ResponsiveContainer>
-      <List>
-        <NewsItem dataNews={dataNews} />
-      </List>
+      <List>{isLoading ? <Loader /> : <NewsItem dataNews={dataNews} />}</List>
     </ResponsiveContainer>
   );
 }
