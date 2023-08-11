a76ea5d0e04080c486726e97685f1f0478ffca2a
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-08-11 23:02:27 +0300

    FIX: fonts

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import Unfonts from 'unplugin-fonts/vite';
+
 
 export default defineConfig({
   plugins: [
     react(),
+    Unfonts({ 
+      google: {
+        families: ['Manrope']}
+    }),
     svgr({
       svgrOptions: {
         // svgr options
